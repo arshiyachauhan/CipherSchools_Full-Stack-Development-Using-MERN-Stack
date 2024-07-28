4efbdfa4f3f87ceaa1829df25dd83d63ffd142c8
Arshiya Chauhan <119539567+arshiyachauhan@users.noreply.github.com>
2024-07-29 00:23:32 +0530

    creating server

--- Lecture-31-CipherSchools/app.js
+++ Lecture-31-CipherSchools/app.js
@@ -3,7 +3,6 @@ const http = require("http");
 const server = http.createServer((req,res) => {
     res.write("This is the response from your first node.js server");
     res.end();
-
 })
 server.listen(3000, ()=> {
     console.log(`Server is running`);
